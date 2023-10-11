dbff220c13ddb6cb4dea19058b0243708f2c9ab1
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2023-10-10 19:14:26 -0500

    serial debugging

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "JSONParser.h"
-
-int main(){
-    SD.begin(BUILTIN_SDCARD);
-    Serial.begin(9600);
-    json_parser::arduino_parser parser = json_parser::arduino_parser("device/config/sensors.config");
-    Serial.println(parser.get("BMP388_SENSOR_FILE_PATH"));
-    while(true){}
-}
